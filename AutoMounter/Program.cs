ebca23a187fd17a59e0dfbd70f99b3da0b6dd8ac
Pedro <pedroignacioperezvargas@gmail.com>
2018-08-10 21:27:46 -0300

    Add project files.

---
+++ AutoMounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMounter {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
